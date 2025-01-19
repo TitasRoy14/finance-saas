2fda51aa57d56f4a3e9d532081ef490eccd54426
TitasRoy14 <roytitas800@gmail.com>
2025-01-19 11:58:30 +0530

    Implement dashboard charts and pie

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { DataCharts } from '@/components/data-charts';
 import DataGrid from '@/components/data-grid';
 import { useGetSummary } from '@/features/summary/api/use-get-summary';
 import { formatCurrency } from '@/lib/utils';
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
       <DataGrid />
+      <DataCharts />
     </div>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -22,6 +22,7 @@ export const Header = () => {
           </ClerkLoading>
         </div>
         <WelcomeMsg />
+        <
       </div>
     </header>
   );
