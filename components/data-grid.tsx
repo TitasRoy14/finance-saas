b5de1764914eb6e765c4570601646e4aadf915b1
TitasRoy14 <roytitas800@gmail.com>
2025-06-15 20:52:43 +0530

    Something change

--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -4,6 +4,7 @@ import { DataCard, DataCardLoading } from '@/components/data-card';
 import { useGetSummary } from '@/features/summary/api/use-get-summary';
 import { formatDateRange } from '@/lib/utils';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 import { FaPiggyBank } from 'react-icons/fa';
 import { FaArrowTrendDown, FaArrowTrendUp } from 'react-icons/fa6';
 
@@ -17,41 +18,45 @@ const DataGrid = () => {
 
   if (isLoading) {
     return (
-      <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8'>
-        <DataCardLoading />
-        <DataCardLoading />
-        <DataCardLoading />
-      </div>
+      <Suspense>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+          <DataCardLoading />
+          <DataCardLoading />
+          <DataCardLoading />
+        </div>
+      </Suspense>
     );
   }
 
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8'>
-      <DataCard
-        title='Remaining'
-        value={data?.remainingAmount}
-        percentageChange={data?.remainingChange}
-        icon={FaPiggyBank}
-        variant='bluish'
-        dateRange={dateRangeLabel}
-      />
-      <DataCard
-        title='Income'
-        value={data?.incomeAmount}
-        percentageChange={data?.incomeChange}
-        icon={FaArrowTrendUp}
-        variant='bluish'
-        dateRange={dateRangeLabel}
-      />
-      <DataCard
-        title='Expenses'
-        value={data?.expensesAmount}
-        percentageChange={data?.expensesChange}
-        icon={FaArrowTrendDown}
-        variant='bluish'
-        dateRange={dateRangeLabel}
-      />
-    </div>
+    <Suspense>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+        <DataCard
+          title="Remaining"
+          value={data?.remainingAmount}
+          percentageChange={data?.remainingChange}
+          icon={FaPiggyBank}
+          variant="bluish"
+          dateRange={dateRangeLabel}
+        />
+        <DataCard
+          title="Income"
+          value={data?.incomeAmount}
+          percentageChange={data?.incomeChange}
+          icon={FaArrowTrendUp}
+          variant="bluish"
+          dateRange={dateRangeLabel}
+        />
+        <DataCard
+          title="Expenses"
+          value={data?.expensesAmount}
+          percentageChange={data?.expensesChange}
+          icon={FaArrowTrendDown}
+          variant="bluish"
+          dateRange={dateRangeLabel}
+        />
+      </div>
+    </Suspense>
   );
 };
 
